d250b8be1e8b9e0739af655100ab7091c70e9ad6
Marycorreia12 <correia_nety@hotmail.com>
2020-04-13 12:04:31 +0100

    Criando as camadas mvc(model, view e controller)

---
+++ main/java/com/example/eventoapp/controllers/EventoController.java
@@ -0,0 +1,14 @@
+package com.example.eventoapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+	
+	@RequestMapping("/cadastrar")
+	public String form() {
+		return "evento/formEvento";
+	}
+
+}
